0f0c524c1c4f813cc9a8bde57d4e699f3c7677ea
joshcano <joshcano@gmail.com>
2018-03-17 00:32:31 -0600

    Add goal and user_id relation to posts model

--- db/migrate/20180317031817_create_posts.rb
+++ db/migrate/20180317031817_create_posts.rb
@@ -3,6 +3,8 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.text :body
       t.string :title
+      t.integer :user_id
+      t.float :goal
 
       t.timestamps
     end
