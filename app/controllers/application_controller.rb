22c58c6c8c8a2fee28260d8d0ff8548fa190f7a6
joshcano <joshcano@gmail.com>
2018-03-17 14:02:16 -0600

    Add in Error catch to alert user that what they did was wrong

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+
+    flash[:error] = "You do not have rights to do this."
+    redirect_to(request.referrer || root_path)
+  end
 end
