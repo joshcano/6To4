b2c56792f57abab0eb5173a964b3ef33a2d090ca
joshcano <joshcano@gmail.com>
2018-03-17 00:33:10 -0600

    Create Seed file so we dont have to nuke the db every time we need new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+users = %w[josh@example.com matt@example.com james@example.com]
+users.each do |user|
+  new_user =  User.new(email: user, password: 'password')
+  new_user.save!
+  new_user.posts.create(title: 'test title', body: 'test body', goal: '100')
+end
+
